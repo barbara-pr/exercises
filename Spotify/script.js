74a895eb38a8d71b0ddfc55e0fd54e5d9c5504e8
barbara-pr <baarbarapereirar@gmail.com>
2024-08-03 13:23:08 -0300

    new

--- Spotify/script.js
+++ Spotify/script.js
@@ -5,38 +5,51 @@ const cover = document.getElementById('cover');
 const play = document. getElementById('play');
 const next = document. getElementById('next');
 const previous = document. getElementById('previous');
+const likeButton = document. getElementById('like');
 const currentProgress = document. getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
 const shuffleButton = document.getElementById('shuffle');
+const repeatButton = document.getElementById('repeat');
+const songTime = document.getElementById('song-time');
+const totalTime = document.getElementById('total-time');
 
 const underPressure = {
     songName: 'Under Pressure',
     artist: 'Queen',
-    file: 'Queen - Under Pressure (Lyrics)'
+    file: 'Queen - Under Pressure (Lyrics)',
+    liked: false
 };
 
 const hotelCalifornia = {
     songName: 'Hotel California',
     artist: 'Eagles',
-    file: 'Hotel California (2013 Remaster)'
+    file: 'Hotel California (2013 Remaster)',
+    liked: false
 };
 
 const itsMyLife = {
     songName: 'It\'s My Life',
     artist: 'Bon Jovi',
-    file: 'It\'s My Life'
+    file: 'It\'s My Life',
+    liked: false
 };
 
 const andILoveHer = {
     songName: 'And I Love Her',
     artist: 'The Beatles',
-    file: 'And I Love Her'
+    file: 'And I Love Her',
+    liked: false
 };
 
+// Variáveis auxiliares que ajudam a visualizar se a função está ou não habilitada
 let isPlaying = false;
 let isShuffled = false;
+let repeatOn = false;
+// O botão de 'like' deve gerenciar o estado de cada música individualmente. Se uma música for descurtida, isso não deve afetar as outras, por isso vamos definir a variável auxiliar para cada uma
 
-const originalPlaylist = [underPressure, hotelCalifornia, itsMyLife, andILoveHer]; // Referência de como é a ordem originalmente
+// Se a playlist armazenada não existir, o operador '??' fornece uma lista padrão.
+const originalPlaylist = JSON.parse(localStorage.getItem('playlist')) ?? [underPressure, hotelCalifornia, itsMyLife, andILoveHer];
+/* const originalPlaylist = [underPressure, hotelCalifornia, itsMyLife, andILoveHer]; --> Referência de como é a ordem originalmente */
 let sortedPlaylist = [...originalPlaylist]; // Espalhando o conteúdo do array original
 let index = 0;
 
@@ -66,11 +79,25 @@ function playPauseDecider(){
     }
 }
 
+// Função para estilizar o botão de 'like'
+function likeButtonRender(){
+    if(sortedPlaylist[index].liked === true){
+        likeButton.querySelector('.bi').classList.remove('bi-heart');
+        likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+        likeButton.classList.add('button-active');
+    } else{
+        likeButton.querySelector('.bi').classList.add('bi-heart');
+        likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+        likeButton.classList.remove('button-active');
+    }
+}
+
 function initializeSong(){
     cover.src = `images/${sortedPlaylist[index].file}.jpg`;
     song.src = `songs/${sortedPlaylist[index].file}.mp4`;
     songName.innerText = sortedPlaylist[index].songName;
     bandName.innerText = sortedPlaylist[index].artist;
+    likeButtonRender();
 }
 
 function previousSong(){
@@ -95,13 +122,15 @@ function nextSong(){
     playSong();
 }
 
-function updateProgressBar(){
+// Função para atualizar a barra de progresso e o tempo
+function updateProgress(){
     /*
     song.currentTime // Quantos segundos já foram tocados
     song.duration // Duração total da música em segundos
     */
     const barWidth = (song.currentTime/song.duration)*100; // Dividindo o que foi tocado pelo total para encontrar o percentual da largura
     currentProgress.style.setProperty('--progress',`${barWidth}%`) // Mudando a propriedade
+    songTime.innerText = toHHMMSS(song.currentTime);
 }
 
 // Função para o user pular para determinada parte da música
@@ -128,15 +157,65 @@ function shuffleArray(preShuffleArray){ // Ela quer um array antes de ser embara
 
 function shuffleButtonClicked(){
     if(isShuffled === false){
-        isShuffled = true;
-        shuffleArray(sortedPlaylist); // Embaralha o sortedPlaylist
-        shuffleButton.classList.add('button-active'); // Lista das classes 
+        isShuffled = true; // Afirmando que, após o clique, está sim embaralhado         
+        shuffleArray(sortedPlaylist); // Embaralha de fato o sortedPlaylist
+        shuffleButton.classList.add('button-active'); // Pego a lista de classes que ele tem e adiciono mais uma classe à ela (que contém o estilo do botão após ser embaralhado)
     }
     else{
         isShuffled = false;
-        sortedPlaylist = [...originalPlaylist]; // Voltar para a ordenação final
-        shuffleButton.classList.remove('button-active');
+        sortedPlaylist = [...originalPlaylist]; // Desembaralha
+        shuffleButton.classList.remove('button-active'); // Desativa o CSS anterior
     }
+}
+
+function repeatButtonClicked(){
+    if(repeatOn === false){
+        repeatOn = true;
+        repeatButton.classList.add('button-active'); 
+    }
+    else{
+        repeatOn = false;
+        repeatButton.classList.remove('button-active');
+    }
+}
+
+function nextOrRepeat(){
+    if(repeatOn === false){
+        nextSong();
+    }
+    else{
+        playSong();
+    }
+}
+
+// Função para formatar o tempo (em string)
+function toHHMMSS(originalNumber){
+    // Math.floor só usa o que vem antes da vírgula
+    let hours = Math.floor(originalNumber / 3600); // Número de horas 
+    let min = Math.floor((originalNumber - hours * 3600) / 60);
+    let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
+
+    return `${hours.toString().padStart(2, '0')}:${min
+        .toString()
+        .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+}
+
+function updateTotalTime(){
+    toHHMMSS(song.duration);
+    totalTime.innerText = toHHMMSS(song.duration);
+}
+
+function likeButtonClicked(){
+    if(sortedPlaylist[index].liked === false){
+        sortedPlaylist[index].liked = true;
+    }else{
+        sortedPlaylist[index].liked = false;
+    }
+    likeButtonRender();
+
+    // BACK-END: O botão de 'like' exige um armazenamente daquela informação, o like deve ficar registrado num banco de dados. Os navegadores tem o recurso de localStorage: armazenamente local do navegador
+    // 'setItem' = registre um item (PARÂMETROS: nome dado ao item e quem é esse item)
+    localStorage.setItem('playlist', JSON.stringify(originalPlaylist));
 }
 
 initializeSong();
@@ -144,6 +223,10 @@ initializeSong();
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
-song.addEventListener('timeupdate', updateProgressBar); // Evento para reagir ao progresso da música
+song.addEventListener('timeupdate', updateProgress); // Evento para reagir ao progresso da música
+song.addEventListener('ended', nextOrRepeat); // Evento para quando a música acabar
+song.addEventListener('loadedmetadata', updateTotalTime);
 progressContainer.addEventListener('click', jumpTo);
 shuffleButton.addEventListener('click', shuffleButtonClicked);
+repeatButton.addEventListener('click', repeatButtonClicked);
+likeButton.addEventListener('click', likeButtonClicked);
